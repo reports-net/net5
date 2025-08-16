45a443ca19d754765282ee467723a2049bf42c9f
paopapa <pao@pao.ac>
2025-08-17 03:25:19 +0900

    Add initial project files and structure

---
+++ 03.帳票WEBアプリケーション－各種クラウド公開動作中/CS/PaoWebApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace PaoWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+
+                    // シンプルなDockerFileをビルド手法で他の環境において動作させる時
+                    // ポートを80から変更するときにコメントアウトて、お好みのポートを指定してください。
+                    //webBuilder.UseUrls("http://0.0.0.0:5000");  // ADD IMPORTANT !!
+
+
+                    // シンプルなDockerFileビルドでAWSやGCPその他環境で動作させる手法については、
+                    // 次のURLの動画を参考にされてください。基本的には同じ手順です。
+                    // AWS-EC2で動作(.NET5) https://youtu.be/0y3K3CW7DRM
+                    // AWS-EC2で動作(.NET6) https://youtu.be/UnPXcadLwFY
+                    // GCPで動作 https://youtu.be/0y3K3CW7DRM
+
+                });
+
+    }
+}
---
+++ 04.帳票作成WEB API－各種クラウドに配置・連携中/CS/Server/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PaoWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+
+            // Swagger（.NET 5では AddEndpointsApiExplorer は不要）
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
